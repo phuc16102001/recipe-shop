21927dc4175234536acafbb1e776126b777068a0
phuc16102001 <phuc16102001@gmail.com>
2022-08-05 17:16:59 +0700

    Add list recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Phở bò',
+      'Mô tả',
+      'https://ngonaz.com/wp-content/uploads/2021/09/cach-nau-pho-bo-nam-dinh-1.jpg'
+    ),
+    new Recipe(
+      'Phở bò',
+      'Mô tả',
+      'https://ngonaz.com/wp-content/uploads/2021/09/cach-nau-pho-bo-nam-dinh-1.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
